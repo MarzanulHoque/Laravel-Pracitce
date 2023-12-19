e04f51e2a33f78bea164e8278e31e4dfa37bf3d2
marzanulhoque <marzanulru17@gmail.com>
2023-12-19 07:55:24 +0600

    Laravel Book Review App with Star rating system

---
+++ book-review/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Book::factory(33)->create()->each(function($book){
+
+            $numsReviews = random_int(5,30);
+
+            Review::factory()->count($numsReviews)
+                    ->good()
+                    ->for($book)
+                    ->create();
+
+        });
+
+        Book::factory(33)->create()->each(function($book){
+
+            $numsReviews = random_int(5,30);
+
+            Review::factory()->count($numsReviews)
+                    ->average()
+                    ->for($book)
+                    ->create();
+
+        });
+
+        Book::factory(34)->create()->each(function($book){
+
+            $numsReviews = random_int(5,30);
+
+            Review::factory()->count($numsReviews)
+                    ->bad()
+                    ->for($book)
+                    ->create();
+
+        });
+    }
+}
